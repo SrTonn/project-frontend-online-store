ae2ba2b13af255f029e54510d00ea018a400f3da
RosieBaroni <rosi.baroni@gmail.com>
2022-02-09 19:33:01 -0300

    Criação do componente Categories e feita a requisição da lista de categorias via API na página principal

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import Input from '../../components/Input';
 import styles from './styles.module.css';
+import Categories from '../../components/Categories';
+import { getCategories } from '../../services/api';
 
 export default class Home extends Component {
+  state = {
+    categories: [],
+  }
+
+  async componentDidMount() {
+    const list = await getCategories();
+
+    this.setState({ categories: list });
+  }
+
   render() {
+    const {
+      categories,
+    } = this.state;
+
     return (
       <>
         <Input
@@ -16,6 +32,8 @@ export default class Home extends Component {
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+
+        <Categories categories={ categories } />
       </>
     );
   }
