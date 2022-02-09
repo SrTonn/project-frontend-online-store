386e8d3c797f3008ff25635551203aff9b15a53a
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-02-09 16:25:06 -0300

    nome da prop alterada de handleChange p/ onChange

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ export default class App extends React.Component {
           render={ () => (
             <Home
               { ...this.state }
-              handleChange={ this.handleChange }
+              onChange={ this.handleChange }
             />) }
         />
       </Switch>
