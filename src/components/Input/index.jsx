e3c3d64672e30f35360b4c87d4c4453dbfe83663
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-02-09 15:45:51 -0300

    componente Input criado

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+export default class Input extends Component {
+  render() {
+    return (
+      <div>index</div>
+    );
+  }
+}
