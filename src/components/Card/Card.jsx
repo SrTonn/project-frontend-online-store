047d2077f4c8ac94c64f9f21e1b0e258608b47a0
Felippe Fernandes <felippe1000@hotmail.com>
2022-02-11 17:17:27 -0300

    Removendo Link duplicado

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import styles from './styles.module.css';
 
 export default class Card extends Component {
