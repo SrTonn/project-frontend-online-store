a9c1fab3810d788dae500e1ec6475945eb14abab
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-02-11 15:32:11 -0300

    componente ProductCart criado

---
+++ src/components/ProductCart/ProductCart.jsx
@@ -0,0 +1,65 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import styles from './styles.module.css';
+
+export default class ProductCart extends Component {
+  render() {
+    const { id, title, imageUrl, price, quantity, updateCartItem } = this.props;
+    return (
+      <div className={ styles.CartItemContainer }>
+        <button
+          onClick={ updateCartItem }
+          type="button"
+          id={ id }
+          name="remove"
+        >
+          remover
+        </button>
+        <img src={ imageUrl } className={ styles.TagImg } alt={ title } />
+        <div
+          style={ {
+            display: '-webkit-box',
+            overflow: 'hidden',
+            '-webkit-line-clamp': '2',
+            '-webkit-box-orient': 'vertical',
+          } }
+        >
+          <abbr title={ title }>{title}</abbr>
+        </div>
+        <span>-</span>
+        <button
+          onClick={ updateCartItem }
+          type="button"
+          id={ id }
+          data-testid="product-decrease-quantity"
+          name="less"
+        >
+          Remover
+        </button>
+        <span>{quantity}</span>
+        <button
+          type="button"
+          id={ id }
+          name="add"
+          data-testid="product-increase-quantity"
+          onClick={ updateCartItem }
+        >
+          Adicionar
+        </button>
+        <span>+</span>
+        <span className={ styles.Price }>
+          {price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+        </span>
+      </div>
+    );
+  }
+}
+
+ProductCart.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  quantity: PropTypes.number.isRequired,
+  updateCartItem: PropTypes.func.isRequired,
+};
