3ea3a627fbf668f5ed0b082bfea66bc2dffcde40
joekliws <j.wilk@hotmail.com>
2022-02-14 17:36:06 -0300

    correções lint

--- src/components/PaymentMethod/PaymentMethod.jsx
+++ src/components/PaymentMethod/PaymentMethod.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styles from './styles.module.css';
+// import styles from './styles.module.css';
 
 class PaymentMethod extends Component {
   render() {
--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import BuyerInfo from '../../components/BuyerInfo/BuyerInfo';
 import CheckoutProduts from '../../components/CheckoutProducts/CheckoutProduts';
 import PaymentMethod from '../../components/PaymentMethod/PaymentMethod';
 import styles from './style.module.css';
 
 class Checkout extends Component {
-  state = {
-    fullName: '',
-    email: '',
-    cpf: '',
-    phone: '',
-    cep: '',
-    address: '', 
-  }
+  // state = {
+  //   fullName: '',
+  //   email: '',
+  //   cpf: '',
+  //   phone: '',
+  //   cep: '',
+  //   address: '',
+  // }
 
   render() {
-    const { location: { state }  } = this.props;
-    console.log(state);
+    const { location: { state } } = this.props;
     return (
       <div className={ styles.CheckoutContainer }>
         <div>voltar</div>
@@ -27,5 +27,12 @@ class Checkout extends Component {
     );
   }
 }
+
+Checkout.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string,
+    state: PropTypes.arrayOf(PropTypes.object).isRequired,
+  }).isRequired,
+};
 
 export default Checkout;
