5a4f71431da5dd2a05b3dc92f0caa27d32714709
joekliws <j.wilk@hotmail.com>
2022-02-17 10:07:49 -0300

    implementar função para redirecionar para checkout

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -5,53 +5,59 @@ import styles from './styles.module.css';
 import { CartButton } from '../../components/CartButton/CartButton';
 
 export default class Cart extends Component {
-  render() {
-    const { cartProductList, history: { goBack } } = this.props;
+handleGoToCheckout = () => {
+  const { history: { push } } = this.props;
+  push('/checkout');
+}
 
-    return (
-      <div className={ styles.CartContainer }>
-        <button type="button" onClick={ goBack }>go back</button>
-        <CartButton className={ styles.CartButton } />
-        <h2>Carrinho de Compras</h2>
+render() {
+  const { cartProductList, history: { goBack } } = this.props;
 
-        <section className={ styles.CartItemsContainer }>
-          {cartProductList.length === 0
-            ? <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p> : (
-              cartProductList.map((product) => (
-                <ProductCart
-                  key={ product.id }
-                  id={ product.id }
-                  imageUrl={ product.imageUrl }
-                  title={ product.title }
-                  quantity={ product.quantity }
-                  price={ product.totalPrice }
-                  { ...this.props }
-                />
-              ))
-            )}
-        </section>
+  return (
+    <div className={ styles.CartContainer }>
+      <button type="button" onClick={ goBack }>go back</button>
+      <CartButton className={ styles.CartButton } />
+      <h2>Carrinho de Compras</h2>
 
-        <h2>
-          Valor Total da Compra:
-          {' '}
-          {cartProductList.reduce((acc, item) => acc + item.totalPrice, 0)
-            .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
-        </h2>
-        <button
-          type="button"
-          data-testid="checkout-products"
-          onClick={ this.handleClick }
-        >
-          Finalizar Compra
-        </button>
-      </div>
-    );
-  }
+      <section className={ styles.CartItemsContainer }>
+        {cartProductList.length === 0
+          ? <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p> : (
+            cartProductList.map((product) => (
+              <ProductCart
+                key={ product.id }
+                id={ product.id }
+                imageUrl={ product.imageUrl }
+                title={ product.title }
+                quantity={ product.quantity }
+                price={ product.totalPrice }
+                { ...this.props }
+              />
+            ))
+          )}
+      </section>
+
+      <h2>
+        Valor Total da Compra:
+        {' '}
+        {cartProductList.reduce((acc, item) => acc + item.totalPrice, 0)
+          .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+      </h2>
+      <button
+        type="button"
+        data-testid="checkout-products"
+        onClick={ this.handleGoToCheckout }
+      >
+        Finalizar Compra
+      </button>
+    </div>
+  );
+}
 }
 
 Cart.propTypes = {
   cartProductList: PropTypes.arrayOf(PropTypes.object).isRequired,
   history: PropTypes.shape({
     goBack: PropTypes.func.isRequired,
+    push: PropTypes.func,
   }).isRequired,
 };
